67364625e6ec7beac00dc3fd5156422f5d65a224
LEEPHILHO <dlvlfgh010@gmail.com>
2021-11-07 23:52:28 +0900

    feat: Print Conda after Odd Number

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
 print('Hello World!')
 
+
 for i in range(5):
     print('Python works.')
 
-    
+
+for j in range(1, 15):
+    if j%2==1:
+        print('{} conda'.format(j))
+    else:
+        print('{}'.format(j))
