1933c180333662b406416380a06b0d556f088d7b
LEEPHILHO <dlvlfgh010@gmail.com>
2021-11-07 23:12:00 +0900

    feat: Print Python after Even Number
    if only i is even
    else, print number

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
 print('Hello World!')
 
+
 for i in range(5):
     print('Python works.')
 
-    
+
+for i in range(10):
+    if i %2==0:
+        print(f'{i} python')
+    else:
+        print(f'{i}')
+
